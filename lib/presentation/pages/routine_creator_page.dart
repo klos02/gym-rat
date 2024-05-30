f735b143f9181e2ab8ebb91e7d7728ffe0c8b9b7
klos02 <bartoszklosinski02@gmail.com>
2024-05-30 03:49:50 +0200

    Routine creator: added reps, weight and set type

--- lib/presentation/pages/routine_creator_page.dart
+++ lib/presentation/pages/routine_creator_page.dart
@@ -6,6 +6,7 @@ import 'package:gym_rat/presentation/pages/selected_exercise_page.dart';
 
 List<ExerciseData> exercises = [];
 List<int> setsList = List.filled(20, 0);
+Map<int, List<String>> setsOptions = {};
 
 Future<void> addExerciseToList(ExerciseData exercise) {
   exercises.add(exercise);
@@ -37,6 +38,58 @@ class _RoutineCreatorPageState extends State<RoutineCreatorPage> {
     setState(() {
       _exercises = exercises;
     });
+  }
+
+  void _showOptionsDialog(int exerciseIndex, int setIndex) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Select Option"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: Text("Failure"),
+                onTap: () {
+                  setState(() {
+                    setsOptions[exerciseIndex]![setIndex] = 'F';
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                title: Text("Warm up"),
+                onTap: () {
+                  setState(() {
+                    setsOptions[exerciseIndex]![setIndex] = 'W';
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                title: Text("Dropset"),
+                onTap: () {
+                  setState(() {
+                    setsOptions[exerciseIndex]![setIndex] = 'D';
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                title: Text("Basic"),
+                onTap: () {
+                  setState(() {
+                    setsOptions[exerciseIndex]![setIndex] = '';
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -114,6 +167,10 @@ class _RoutineCreatorPageState extends State<RoutineCreatorPage> {
                               onPressed: () {
                                 setState(() {
                                   setsList[index]++;
+                                  if (setsOptions[index] == null) {
+                                    setsOptions[index] = [];
+                                  }
+                                  setsOptions[index]!.add('');
                                 });
                               },
                             ),
@@ -142,10 +199,52 @@ class _RoutineCreatorPageState extends State<RoutineCreatorPage> {
                             itemBuilder: (context, innerIndex) {
                               return Row(
                                 children: [
-                                  Expanded(
+                                  GestureDetector(
+                                    onTap: () {
+                                      _showOptionsDialog(index, innerIndex);
+                                    },
+                                    child: Container(
+                                      width: 30,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.black),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          '${innerIndex + 1}${setsOptions[index]?[innerIndex] ?? ''}',
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Container(
+                                    width: 40,
                                     child: TextFormField(
                                       decoration: InputDecoration(
-                                        hintText: 'Ilość powtórzeń',
+                                        hintText: 'Rep',
+                                        contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 4.0),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Container(
+                                    width: 60,
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: 'Weight',
+                                        contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 4.0),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0),
+                                        ),
                                       ),
                                     ),
                                   ),
@@ -155,10 +254,12 @@ class _RoutineCreatorPageState extends State<RoutineCreatorPage> {
                                           onPressed: () {
                                             setState(() {
                                               setsList[index]--;
+                                              setsOptions[index]!
+                                                  .removeAt(innerIndex);
                                             });
                                           },
                                         )
-                                      : Container()
+                                      : SizedBox(width: 4),
                                 ],
                               );
                             },
